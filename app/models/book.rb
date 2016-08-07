70227f397049eaac635f94e714c5e4c865370053
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-08-07 15:58:39 +0300

    add image for book

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,8 @@ class Book < ActiveRecord::Base
   validates :title, :description, :author, :user_id, :category_id, presence: true
   validates :title, :author, length: { minimum: 2, maximum: 255 }
   validates :description, length: { minimum: 127 }
+
+  has_attached_file :book_img, styles: { book_index: "250x350>", book_show: "325x475>" },
+                    default_url: ':style/missing.png'
+  validates_attachment_content_type :book_img, content_type: /\Aimage\/.*\Z/
 end
