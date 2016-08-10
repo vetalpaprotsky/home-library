34aad2b4999af62d0a7a0c679fde21f60598f990
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-08-10 22:25:02 +0300

    add review model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
+  has_many :reviews
   validates :title, :description, :author, :user_id, :category_id, presence: true
   validates :title, :author, length: { minimum: 2, maximum: 255 }
   validates :description, length: { minimum: 127 }
