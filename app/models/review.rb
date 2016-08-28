6031f9c3eae47e4c268ea06b033810e08c389d77
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-08-28 12:02:03 +0300

    add tests for Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :book
+
+  validates :rating, :comment, :book_id, :user_id, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 1,
+                                        less_than_or_equal_to: 5 }
 end
