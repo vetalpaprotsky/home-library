ed62406f37b6bb454593ebefb780caa495b82716
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-09-10 14:32:39 +0300

    add categories to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create([
+  {name: "Science fiction"},
+  {name: "Satire"},
+  {name: "Drama"},
+  {name: "Action and Adventure"},
+  {name: "Romance"},
+  {name: "Mystery"},
+  {name: "Horror"},
+  {name: "Self help"},
+  {name: "Health"},
+  {name: "Guide"},
+  {name: "Travel"},
+  {name: "Children's"},
+  {name: "Religion, Spirituality & New Age"},
+  {name: "Science"},
+  {name: "History"},
+  {name: "Math"},
+  {name: "Anthology"},
+  {name: "Poetry"},
+  {name: "Encyclopedias"},
+  {name: "Dictionaries"},
+  {name: "Comics"},
+  {name: "Art"},
+  {name: "Cookbooks"},
+  {name: "Diaries"},
+  {name: "Journals"},
+  {name: "Prayer books"},
+  {name: "Series"},
+  {name: "Trilogy"},
+  {name: "Biographies"},
+  {name: "Autobiographies"},
+  {name: "Fantasy"}
+])
