3f7dbc07c502d5fc6eab89591902808c53238051
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-09-10 12:53:02 +0300

    add dependent: :destroy for models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   validates :title, :description, :author, :user_id, :category_id, presence: true
   validates :title, :author, length: { minimum: 2, maximum: 255 }
   validates :description, length: { minimum: 127 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :books
-  has_many :reviews
+  has_many :books, dependent: :destroy
+  has_many :reviews, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
