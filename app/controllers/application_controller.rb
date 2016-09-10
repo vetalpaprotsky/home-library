0c8cc3f20b5c3d687cac8887314f78fd2971b8c8
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-09-10 22:32:17 +0300

    add internationalization to app and translate some links on ukrainian

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  # later users will have column "language" where they store their own languages
+  # they will be able to choose language in settings
+  # def set_locale
+  #   I18n.locale = current_user.try(:locale) || I18n.default_locale
+  # end
+
+  def default_url_options(options={})
+    { locale: I18n.locale }.merge options
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  root "books#index"
-  devise_for :users
-  resources :books do
-    resources :reviews, except: [:index, :show], shallow: true
+  get '/' => 'books#index'
+
+  scope "/:locale" do
+    root 'books#index'
+    devise_for :users
+    resources :books do
+      resources :reviews, except: [:index, :show], shallow: true
+    end
   end
 end
