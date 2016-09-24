fc99e71bfd79b710794dc79c4f116d8fcd81b666
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-09-24 23:02:42 +0300

    add averange evaluation for book on book show page

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,4 +10,17 @@ class Book < ActiveRecord::Base
                     default_url: ':style/missing.png'
   validates_attachment_content_type :book_img, content_type: /\Aimage\/(jpg|jpeg|png)\z/
   validates_with AttachmentSizeValidator, attributes: :book_img, less_than: 1.megabytes
+
+  def average_evaluation
+    number_of_evaluations = summa_of_evaluations = 0
+    self.evaluations.each do |evl|
+      summa_of_evaluations += evl.value
+      number_of_evaluations += 1
+    end
+    begin
+      summa_of_evaluations.to_f / number_of_evaluations
+    rescue
+      0
+    end
+  end
 end
