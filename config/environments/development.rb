5df43d755b677d3c01f189a6bddd24f43c545c8d
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-09-29 22:34:38 +0300

    set new password for action mailer in development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
     :port                 => 587,
     :domain               => "gmail.com",
     :user_name            => "vetalpaprotsky@gmail.com",
-    :password             => "37zudohov",
+    :password             => "ultra9mag837zudohov",
     :authentication       => "plain"
   }
 
