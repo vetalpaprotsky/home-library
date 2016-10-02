cac2a531ea6c45d724a80a22026a6921ec979bf6
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-10-02 11:41:30 +0300

    add tests for LocalesController

--- spec/controllers/locales_controller_spec.rb
+++ spec/controllers/locales_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 describe LocalesController do
-  # TODO: write tests
+
+  shared_examples 'change locale' do
+
+    describe '#GET change_locale' do
+
+      it 'redirects to root with new locale' do
+        get :change_locale, language_abbr: 'ua'
+
+        expect(response).to redirect_to root_path(locale: 'ua')
+      end
+    end
+  end
+
+  context 'SIGNED OUT' do
+
+    include_examples 'change locale'
+  end
+
+  context 'SIGNED IN' do
+
+    before { sign_in FactoryGirl.create(:user) }
+
+    include_examples 'change locale'
+  end
 end
