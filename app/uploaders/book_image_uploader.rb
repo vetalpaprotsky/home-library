fef90e2e259d3059e55ff55276843ffaa00ff576
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-10-09 19:42:26 +0300

    add default image for book

--- app/uploaders/book_image_uploader.rb
+++ app/uploaders/book_image_uploader.rb
@@ -17,12 +17,11 @@ class BookImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    # For Rails 3.1+ asset pipeline compatibility:
+    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    "/assets/#{model.class.to_s.underscore}/default.jpg"
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
