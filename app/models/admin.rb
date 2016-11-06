9cf97d6750cb1075ba109b8de5d643ccca1b30db
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-11-06 14:54:35 +0200

    change model Admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,7 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :registerable, :recoverable, :rememberable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :trackable, :validatable
 
   validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
 end
--- db/migrate/20161106101007_devise_create_admins.rb
+++ db/migrate/20161106101007_devise_create_admins.rb
@@ -6,11 +6,11 @@ class DeviseCreateAdmins < ActiveRecord::Migration
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      # t.string   :reset_password_token
+      # t.datetime :reset_password_sent_at
 
       ## Rememberable
-      t.datetime :remember_created_at
+      # t.datetime :remember_created_at
 
       ## Trackable
       t.integer  :sign_in_count, default: 0, null: false
@@ -35,7 +35,7 @@ class DeviseCreateAdmins < ActiveRecord::Migration
     end
 
     add_index :admins, :email,                unique: true
-    add_index :admins, :reset_password_token, unique: true
+    # add_index :admins, :reset_password_token, unique: true
     # add_index :admins, :confirmation_token,   unique: true
     # add_index :admins, :unlock_token,         unique: true
   end
