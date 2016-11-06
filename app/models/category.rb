bff5b7426f92822dbb802a577c9552d115affc48
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-11-06 22:17:46 +0200

    add cacheing of categories and languages

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,18 @@
 class Category < ActiveRecord::Base
   has_many :books
-  
+
   validates :name, presence: true
   default_scope -> { order('name ASC') }
+
+  after_commit :flush_cache
+
+  def self.cached_categories
+    Rails.cache.fetch('all_categories') { self.all.to_a }
+  end
+
+  private
+
+    def flush_cache
+      Rails.cache.delete('all_categories')
+    end
 end
--- app/models/language.rb
+++ app/models/language.rb
@@ -1,5 +1,17 @@
 class Language < ActiveRecord::Base
   validates :name, :abbr, presence: true
-  
+
   default_scope -> { order('abbr ASC') }
+
+  after_commit :flush_cache
+
+  def self.cached_languages
+    Rails.cache.fetch('all_languages') { self.all.to_a }
+  end
+
+  private
+
+    def flush_cache
+      Rails.cache.delete('all_languages')
+    end
 end
