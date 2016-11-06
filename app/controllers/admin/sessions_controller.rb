99c3bff50239efe62b0d533ce1bda8c10cd41c3e
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-11-06 17:54:31 +0200

    add before actions and private methods for Admin::SessionsContoller

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,4 +1,21 @@
 class Admin::SessionsController < Devise::SessionsController
   layout 'admin'
+
+  skip_before_action :set_locale
   before_action Proc.new { I18n.locale = :en }
+  before_action Proc.new { flash.clear }
+
+  private
+
+    def after_sign_out_path_for(resource)
+      new_admin_session_path
+    end
+
+    def after_sign_in_path_for(resource)
+      rails_admin_path
+    end
+
+    def default_url_options(options={})
+      {}.merge options
+    end
 end
