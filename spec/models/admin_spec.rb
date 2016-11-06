00f7bf0ef7388ce77a1d3d74eef70d0966c45cda
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-11-06 14:59:48 +0200

    change tests for model Admin

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -7,15 +7,11 @@ describe Admin do
     it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
     it { is_expected.to have_db_column(:email).of_type(:string) }
     it { is_expected.to have_db_column(:encrypted_password).of_type(:string) }
-    it { is_expected.to have_db_column(:reset_password_token).of_type(:string) }
-    it { is_expected.to have_db_column(:reset_password_sent_at).of_type(:datetime) }
-    it { is_expected.to have_db_column(:remember_created_at).of_type(:datetime) }
     it { is_expected.to have_db_column(:sign_in_count).of_type(:integer) }
     it { is_expected.to have_db_column(:current_sign_in_at).of_type(:datetime) }
     it { is_expected.to have_db_column(:last_sign_in_at).of_type(:datetime) }
     it { is_expected.to have_db_column(:current_sign_in_ip).of_type(:string) }
     it { is_expected.to have_db_column(:last_sign_in_ip).of_type(:string) }
-    
   end
 
   describe 'validations' do
