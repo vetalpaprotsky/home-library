9f3de42aa177d2fa2aa5e8cbbf3ba307ac9b0d27
vitalypaprotsky <vetalpaprotsky@gmail.com>
2016-12-27 23:28:12 +0200

    fix mailer configurations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,15 +13,6 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:               'smtp.gmail.com',
-    port:                   587,
-    domain:                'gmail.com',
-    user_name:             'home.library.website@gmail.com',
-    password:              'ultra9mag837zudohov',
-    authentication:        'plain'
-  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
