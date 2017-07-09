b5d90052ddc99a032f4c9c20b062a3ce00bc221c
VitalyPaprotsky <vetalpaprotsky@gmail.com>
2017-07-09 18:35:48 +0300

    change LocalesController

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -1,6 +1,6 @@
 class LocalesController < ApplicationController
   def change_locale
     cookies[:locale] = params[:language_abbr]
-    redirect_to root_path locale: params[:language_abbr]
+    redirect_to root_path(locale: params[:language_abbr])
   end
 end
